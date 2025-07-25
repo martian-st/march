544c0f2648caf6ecd965c44390cb978bdd031541
sajdakabir <sajda.kbir@gmail.com>
2025-07-25 11:42:17 +0530

    feat: agenda scroll

--- apps/web/src/app/(routes)/agenda/page.tsx
+++ apps/web/src/app/(routes)/agenda/page.tsx
@@ -8,21 +8,28 @@ import { CalendarProvider } from "@/contexts/calendar-context";
 export default function Agenda() {
   return (
     <section className="h-full pl-12">
-      <div className="w-full h-[calc(100vh-64px)] overflow-auto pt-0">
-        <div className="max-w-4xl">
-          {/* Daily Notes Component */}
-          <DailyNotes />
-          
-          {/* List Component without input bar */}
-          <Block id="list-and-calendar" arrayType="today">
-            <GridWrapper>
-              <AgendaListBlock arrayType="today" />
-              {/* Calendar view commented out as requested */}
-              {/* <CalendarProvider>
-                <CalendarBlock />
-              </CalendarProvider> */}
-            </GridWrapper>
-          </Block>
+      <div className="w-full h-[calc(100vh-64px)] flex flex-col pt-0">
+        <div className="max-w-4xl w-full flex flex-col h-full">
+          {/* Split view with two scrollable sections */}
+          <div className="flex flex-col h-full">
+            {/* Daily Notes Component - Scrollable section 1 */}
+            <div className="flex-1 min-h-0 overflow-auto mb-4 border-b border-gray-100 pb-4">
+              <DailyNotes />
+            </div>
+            
+            {/* List Component - Scrollable section 2 */}
+            <div className="flex-1 min-h-0 overflow-auto">
+              <Block id="list-and-calendar" arrayType="today">
+                <GridWrapper>
+                  <AgendaListBlock arrayType="today" />
+                  {/* Calendar view commented out as requested */}
+                  {/* <CalendarProvider>
+                    <CalendarBlock />
+                  </CalendarProvider> */}
+                </GridWrapper>
+              </Block>
+            </div>
+          </div>
         </div>
       </div>
     </section>
